ea8ae2bf71230a866ec538dcec0839eb7668ace5
soledadvega <solmun13@gmail.com>
2019-10-30 12:58:34 -0300

    subiendo vistas

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Question
+from django.template import loader
+'''
 def index(request):
     return HttpResponse("Hola mundo. Estas en el indice de encuestas.")
+'''
+
+def index(request):
+    lastest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in lastest_question_list])
+    return HttpResponse(output)
+
+
+def detail(request, question_id):
+    return HttpResponse("Estas viendo la pregunta %s." % question_id)
+
+def results(request, question_id):
+    response = "Estas viendo los resultados de la pregunta %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Estas votando una pregunta %s." % question_id)
