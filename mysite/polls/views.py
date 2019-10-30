8d3ef2ec027f18bd17613be5780cd0836fdb69bc
soledadvega <solmun13@gmail.com>
2019-10-30 16:19:46 -0300

    subo modificacion

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
 from django.template import loader
+from django.shortcuts import render
+
 '''
 def index(request):
     return HttpResponse("Hola mundo. Estas en el indice de encuestas.")
@@ -10,7 +12,7 @@ def index(request):
     lastest_question_list = Question.objects.order_by('-pub_date')[:5]
     output = ', '.join([q.question_text for q in lastest_question_list])
     return HttpResponse(output)
-'''
+
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -19,7 +21,12 @@ def index(request):
         'latest_question_list': latest_question_list,
     }
     return HttpResponse(template.render(context, request))
+'''
 
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
     return HttpResponse("Estas viendo la pregunta %s." % question_id)
