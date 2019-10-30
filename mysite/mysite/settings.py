3cdf1caa2bce01412d14f015f73391ae2734d41d
soledadvega <solmun13@gmail.com>
2019-10-30 04:16:18 -0300

    subir modelos activados

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig', #para incluir la aplicacion polls
     'django.contrib.admin',  #sitio administrativo
     'django.contrib.auth',  # sistema de autentificacion
     'django.contrib.contenttypes',  #frameworks para tipos de contenido
