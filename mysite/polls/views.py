f96ade38b5a4f80b08c38cebded66752e94aeb6d
soledadvega <solmun13@gmail.com>
2019-10-30 21:48:16 -0300

    subir modificaciones

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,5 @@
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
@@ -28,8 +30,21 @@ def index(request):
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+'''
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
+
 def detail(request, question_id):
     return HttpResponse("Estas viendo la pregunta %s." % question_id)
+'''
 
 def results(request, question_id):
     response = "Estas viendo los resultados de la pregunta %s."
